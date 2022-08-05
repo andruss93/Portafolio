fefec9bdf522d37cfbd7435102ca4a6090ab7a48
andruss93 <you@example.com>
2022-08-04 20:52:04 -0500

    dando acceso a los link de las paginas

--- validacion.js
+++ validacion.js
@@ -1,26 +1,30 @@
-function validarDatos(){
-    window.event.preventDefault()
+// function validarDatos(){
+//     window.event.preventDefault()
      
-    if (document.form.nombre.value=="" ) {
-        alert("Campo nombre es obligatorio")  
-        document.form.nombre.focus()
+//     if (document.form.nombre.value=="" ) {
+//         alert("Campo nombre es obligatorio")  
+//         document.form.nombre.focus()
     
-    }else if (document.form.email.value=="") {
-        alert("Campo e-mail es obligatorio")
-        document.form.email.focus() 
+//     }else if (document.form.email.value=="") {
+//         alert("Campo e-mail es obligatorio")
+//         document.form.email.focus() 
 
-    }else if (document.form.asunto.value=="" ) {
-        alert("Campo Asunto es obligatorio")
-        document.form.asunto.focus() 
+//     }else if (document.form.asunto.value=="" ) {
+//         alert("Campo Asunto es obligatorio")
+//         document.form.asunto.focus() 
 
-    }else if (document.form.mensaje.value=="" || document.form.mensaje.value.length <= 50 ){
-        alert("Campo Mensaje es obligatorio y debe contener máximo 50 carateres") 
-        document.form.mensaje.focus()
+//     }else if (document.form.mensaje.value=="" || document.form.mensaje.value.length <= 50 ){
+//         alert("Campo Mensaje es obligatorio y debe contener máximo 50 carateres") 
+//         document.form.mensaje.focus()
       
-    } else if (document.form.email.value.indexOf('@')==-1 ||
-    document.form.email.value.indexOf('.')==-1 ) {
-        alert("e-mail inválido")
-    }
+//     } else if (document.form.email.value.indexOf('@')==-1 ||
+//     document.form.email.value.indexOf('.')==-1 ) {
+//         alert("e-mail inválido")
+//     }
    
+// }
+//     document.querySelector('form').addEventListener('submit',validarDatos)
+
+function abrirLink(){
+ 
 }
-    document.querySelector('form').addEventListener('submit',validarDatos)
